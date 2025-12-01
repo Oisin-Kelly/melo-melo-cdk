c8ad869a2e076fd881196c652fe9ce3dd34684b0
Oisin Kelly <oisinkelly308@yahoo.co.uk>
2025-12-01 22:49:33 +0000

    feat: added userpool and lambda shells

--- src/MeloMeloCdk/MeloMeloCdkStack.cs
+++ src/MeloMeloCdk/MeloMeloCdkStack.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
 using Amazon.CDK;
-using Amazon.CDK.AWS.AppSync;
+using Amazon.CDK.AWS.Cognito;
 using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.S3;
 using Constructs;
 
@@ -15,13 +18,87 @@ namespace MeloMeloCdk
         private Bucket PrivateReadonlyBucket { get; set; }
         private Bucket PublicReadonlyBucket { get; set; }
         
+        private UserPool UserPool { get; set; }
+        
+        private Function CheckEmailExistenceFunction { get; set; }
+        private Function PostConfirmationFunction { get; set; }
+        
         internal MeloMeloCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             Env = System.Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "dev";
             
             InitialiseTable();
             InitialiseBuckets();
-            
+            InitialiseLambdas();
+            InitialiseCognito();
+        }
+
+        private void InitialiseCognito()
+        {
+            UserPool = new UserPool(this, $"melo-melo-user-pool-{Env}", new UserPoolProps()
+            {
+                SelfSignUpEnabled = true,
+                SignInAliases = new SignInAliases()
+                {
+                    Email = true,
+                    Username = true,
+                },
+                StandardAttributes = new StandardAttributes()
+                {
+                    Email = new StandardAttribute()
+                    {
+                        Required = true,
+                        Mutable = false
+                    }
+                },
+                SignInCaseSensitive = false,
+                UserVerification = new UserVerificationConfig()
+                {
+                    EmailSubject = "Verify your MeloMelo account",
+                    EmailBody = "Verify your MeloMelo account",
+                    EmailStyle = VerificationEmailStyle.CODE
+                },
+                LambdaTriggers = new UserPoolTriggers()
+                {
+                    PostConfirmation = PostConfirmationFunction,
+                    PreSignUp = CheckEmailExistenceFunction,
+                }
+            });
+
+            CheckEmailExistenceFunction.Role!.AttachInlinePolicy(
+                new Policy(this, "userpool-policy", new PolicyProps
+                {
+                    Statements = new[]
+                    {
+                        new PolicyStatement(new PolicyStatementProps
+                        {
+                            Actions = new[] { "cognito-idp:ListUsers" },
+                            Resources = new[] { UserPool.UserPoolArn },
+                            Effect = Effect.ALLOW
+                        })
+                    }
+                })
+            );
+        }
+
+        private void InitialiseLambdas()
+        {
+            var lambdaProps = new FunctionProps()
+            {
+                Runtime = Runtime.DOTNET_8,
+                MemorySize = 1024,
+                Environment = new Dictionary<string, string>()
+                {
+                    { "ENVIRONMENT", Env },
+                    { "TABLE_NAME", DynamoDbTable.TableName },
+                    { "DROPBOX_BUCKET", DropboxBucket.BucketName },
+                    { "PRIVATE_READONLY_BUCKET", PrivateReadonlyBucket.BucketName },
+                    { "PUBLIC_READONLY_BUCKET", PublicReadonlyBucket.BucketName }
+                }
+            };
+
+            CheckEmailExistenceFunction = new Function(this, "CheckEmailExistenceFunction", lambdaProps);
+            PostConfirmationFunction = new Function(this, "PostConfirmationFunction", lambdaProps);
         }
 
         private void InitialiseTable()

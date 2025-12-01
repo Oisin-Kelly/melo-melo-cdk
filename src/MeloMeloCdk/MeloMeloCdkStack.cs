e6734ed77c237eaf190d6c1b885b0c8f0e3f3d0f
Oisin Kelly <oisinkelly308@yahoo.co.uk>
2025-12-01 21:41:45 +0000

    Initial commit

---
+++ src/MeloMeloCdk/MeloMeloCdkStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace MeloMeloCdk
+{
+    public class MeloMeloCdkStack : Stack
+    {
+        internal MeloMeloCdkStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}

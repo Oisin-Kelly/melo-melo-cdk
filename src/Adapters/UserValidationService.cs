e17cd595283caa37e76464eca6a9e3fe7d33010d
Oisin Kelly <oisinkelly308@yahoo.co.uk>
2025-12-03 22:01:26 +0000

    feat: added empty uservalidationservice adapter

---
+++ src/Adapters/UserValidationService.cs
@@ -0,0 +1,16 @@
+﻿using Ports;
+
+namespace Adapters;
+
+public class UserValidationService : IUserValidationService
+{
+    public bool ValidateUsername(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool ValidateEmail(string email)
+    {
+        throw new NotImplementedException();
+    }
+}
